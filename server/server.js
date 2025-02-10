047ab3e001ffd62ff59492d99fc2895594a8cc16
keerthichembayil <keerthichembayil@gmail.com>
2025-02-11 00:39:30 +0530

    "added helloworld"

--- server/server.js
+++ server/server.js
@@ -29,6 +29,9 @@ mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB atlas'))
   .catch((err) => console.error('MongoDB connection error:', err));
+  app.get("/",(req,res)=>{
+    res.json("hello world");
+  })
 
 // Routes
 app.use("/api/auth", authRoutes);
